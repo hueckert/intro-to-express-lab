b0a201d5e664a9c29ed6dd163944fee84edc4546
howard <howard.ueckert@protonmail.com>
2024-11-28 08:46:28 -0600

    task 2

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.get('/greetings/:username',  (req, res) => {
     res.send(`<h1>How you do'n ${req.params.username}.`)
 })
 
+app.get('/roll/:number', (req, res) => {
+    const number = parseInt(req.params.number, 10);
+  
+    if (isNaN(number)) {
+      return res.send("You must specify a number.");
+    }
+  
+    const randomRoll = Math.floor(Math.random() * (number + 1));
+    res.send(`You rolled a ${randomRoll}.`);
+  });
+
 // Listen for requests on port 3000
 app.listen(3000, () => {
     console.log('Listening on port 3000')
