aea709100b2668fef6aa1fc547b29e9d1c24e4ce
howard <howard.ueckert@protonmail.com>
2024-11-28 09:15:40 -0600

    task 3

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ app.get('/roll/:number', (req, res) => {
     res.send(`You rolled a ${randomRoll}.`);
   });
 
+  const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+  app.get("/collectibles/:index", (req, res) => {
+    if (req.params.index >= 0 && req.params.index < collectibles.length) {
+        res.send(`So, you want the ${collectibles[req.params.index].name}? For $${collectibles[req.params.index].price}, it can be yours! `)
+    } else {
+        res.send(`This item is not yet in stock. Check back soon!`)
+    }
+})
+
+
 // Listen for requests on port 3000
 app.listen(3000, () => {
     console.log('Listening on port 3000')
